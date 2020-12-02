d43e5e8649339fead583c8d3eb5f4be420eeb1a6
MaryBloody01 <matba26@gmail.com>
2020-12-02 20:16:09 +0300

    init commit

---
+++ src/main/java/com/example/grpcclient/GrpcclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.grpcclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class GrpcclientApplication {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+
+}
